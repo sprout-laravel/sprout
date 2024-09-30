ceb4654bfb529fa7aceed46b11a4a4388d2816af
Ollie Read <code@ollieread.com>
2024-09-30 15:41:34 +0100

    chore: Update the path and subdomain identity resolvers to update cookie settings on tenancy setup

--- src/Http/Resolvers/SubdomainIdentityResolver.php
+++ src/Http/Resolvers/SubdomainIdentityResolver.php
@@ -8,10 +8,14 @@ use Illuminate\Http\Request;
 use Illuminate\Routing\Router;
 use Illuminate\Routing\RouteRegistrar;
 use Sprout\Concerns\FindsIdentityInRouteParameter;
-use Sprout\Contracts\Tenancy;
 use Sprout\Contracts\IdentityResolverUsesParameters;
+use Sprout\Contracts\Tenancy;
+use Sprout\Contracts\Tenant;
+use Sprout\Exceptions\TenantMissing;
 use Sprout\Http\Middleware\TenantRoutes;
 use Sprout\Support\BaseIdentityResolver;
+use Sprout\Support\CookieHelper;
+use function Sprout\sprout;
 
 final class SubdomainIdentityResolver extends BaseIdentityResolver implements IdentityResolverUsesParameters
 {
@@ -58,7 +62,7 @@ final class SubdomainIdentityResolver extends BaseIdentityResolver implements Id
      *
      * @return string
      */
-    protected function getDomainParameter(Tenancy $tenancy): string
+    protected function getRouteDomain(Tenancy $tenancy): string
     {
         return $this->getRouteParameter($tenancy) . '.' . $this->domain;
     }
@@ -80,10 +84,70 @@ final class SubdomainIdentityResolver extends BaseIdentityResolver implements Id
     public function routes(Router $router, Closure $groupRoutes, Tenancy $tenancy): RouteRegistrar
     {
         return $this->applyParameterPattern(
-            $router->domain($this->getDomainParameter($tenancy))
+            $router->domain($this->getRouteDomain($tenancy))
                    ->middleware([TenantRoutes::ALIAS . ':' . $this->getName() . ',' . $tenancy->getName()])
                    ->group($groupRoutes),
             $tenancy
         );
+    }
+
+    /**
+     * Get the route domain with the parameter replaced with the tenant identifier
+     *
+     * @template TenantClass of \Sprout\Contracts\Tenant
+     *
+     * @param \Sprout\Contracts\Tenancy<TenantClass> $tenancy
+     *
+     * @return string
+     *
+     * @throws \Sprout\Exceptions\TenantMissing
+     */
+    public function getTenantRouteDomain(Tenancy $tenancy): string
+    {
+        if (! $tenancy->check()) {
+            throw TenantMissing::make($tenancy->getName());
+        }
+
+        /** @var string $identifier */
+        $identifier = $tenancy->identifier();
+
+        return str_replace(
+            '{' . $this->getRouteParameterName($tenancy) . '}',
+            $identifier,
+            $this->getRouteDomain($tenancy)
+        );
+    }
+
+    /**
+     * Perform setup actions for the tenant
+     *
+     * When a tenant is marked as the current tenant within a tenancy, this
+     * method will be called to perform any necessary setup actions.
+     * This method is also called if there is no current tenant, as there may
+     * be actions needed.
+     *
+     * @template TenantClass of \Sprout\Contracts\Tenant
+     *
+     * @param \Sprout\Contracts\Tenancy<TenantClass> $tenancy
+     * @param \Sprout\Contracts\Tenant|null          $tenant
+     *
+     * @phpstan-param TenantClass|null               $tenant
+     *
+     * @return void
+     *
+     * @throws \Sprout\Exceptions\TenantMissing
+     */
+    public function setup(Tenancy $tenancy, ?Tenant $tenant): void
+    {
+        parent::setup($tenancy, $tenant);
+
+        if (sprout()->config('services.cookies', false) === true) {
+            if ($tenant !== null) {
+                CookieHelper::setDefaults(domain: $this->getTenantRouteDomain($tenancy));
+            } else {
+                // If there's no tenant, we can set it to its defaults
+                CookieHelper::setDefaults();
+            }
+        }
     }
 }
