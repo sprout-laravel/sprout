9ac6c9949b45a309420c9d9ded379812d5e0d201
Ollie Read <code@ollieread.com>
2024-09-30 15:39:29 +0100

    chore: Update sprout config with comments

--- resources/config/sprout.php
+++ resources/config/sprout.php
@@ -2,10 +2,46 @@
 
 return [
 
+    /*
+    |--------------------------------------------------------------------------
+    | Should Sprout listen for routing?
+    |--------------------------------------------------------------------------
+    |
+    | This value decides whether Sprout listens for the RouteMatched event to
+    | identify tenants.
+    |
+    | Setting it to false will disable the pre-middleware identification,
+    | which in turn will make tenant-aware dependency injection no longer
+    | functionality.
+    |
+    */
+
     'listen_for_routing' => true,
 
+    /*
+    |--------------------------------------------------------------------------
+    | Service Overrides
+    |--------------------------------------------------------------------------
+    |
+    | This value sets which core Laravel services Sprout should override.
+    |
+    | Setting a service to false will disable its tenant-specific
+    | configuration/settings, and leave them using the default.
+    |
+    */
+
     'services' => [
-        'storage' => true,
+        // This will enable the 'sprout' driver for the filesystem disks,
+        // allowing for the creation of tenant scoped disks.
+        'storage'  => true,
+
+        // This will enable the overwriting of the default settings for cookies.
+        // Each identity resolver may have effect different settings.
+        'cookies'  => true,
+
+        // This will enable the overwriting of the default settings for sessions.
+        // Each identity resolver may have effect different settings.
+        'sessions' => true,
     ],
 
 ];
