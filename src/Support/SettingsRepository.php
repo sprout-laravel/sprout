2eeebb20304c40adc920ee93a25fe6e15e5089c3
Ollie Read <code@ollieread.com>
2024-12-16 11:18:41 +0000

    chore: Add core Sprout settings repository

---
+++ src/Support/SettingsRepository.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Sprout\Support;
+
+use Illuminate\Config\Repository;
+
+/**
+ * Settings Repository
+ *
+ * This class is used to store internal Sprout settings used by various
+ * components for various purposes.
+ */
+final class SettingsRepository extends Repository
+{
+
+}
