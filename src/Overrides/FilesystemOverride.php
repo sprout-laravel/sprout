ee1d04e9fa7b853c8e39e3848c9ad5adbea6b435
Ollie Read <code@ollieread.com>
2025-01-12 13:02:58 +0000

    fix: Fix two issues with the filesystem override
    The sprout driver for the cache manager wasn't added if the cache manager had already been resolved, and because the driver creation method was bound to the manager, it wasn't able to access $this->drivers

--- src/Overrides/FilesystemOverride.php
+++ src/Overrides/FilesystemOverride.php
@@ -3,12 +3,16 @@ declare(strict_types=1);
 
 namespace Sprout\Overrides;
 
+use Closure;
+use Illuminate\Cache\CacheManager;
+use Illuminate\Contracts\Cache\Repository;
 use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Filesystem\FilesystemManager;
 use Sprout\Contracts\BootableServiceOverride;
 use Sprout\Contracts\Tenancy;
 use Sprout\Contracts\Tenant;
+use Sprout\Overrides\Cache\SproutCacheDriverCreator;
 use Sprout\Overrides\Filesystem\SproutFilesystemDriverCreator;
 use Sprout\Overrides\Filesystem\SproutFilesystemManager;
 use Sprout\Sprout;
@@ -61,18 +65,29 @@ final class FilesystemOverride extends BaseOverride implements BootableServiceOv
             $app->singleton('filesystem', fn ($app) => new SproutFilesystemManager($app, $original));
         }
 
-        // We only want to add the driver if the filesystem service is
-        // resolved at some point
-        $app->afterResolving('filesystem', function (FilesystemManager $manager) use ($sprout) {
-            $manager->extend('sprout', function (Application $app, array $config) use ($manager, $sprout): Filesystem {
-                // If the config contains the disk name
-                if (isset($config['name'])) {
-                    // Track it
-                    $this->drivers[] = $config['name'];
-                }
+        $tracker = fn(string $store) => $this->drivers[] = $store;
 
-                return (new SproutFilesystemDriverCreator($app, $manager, $config, $sprout))();
+        // If the filesystem manager has been resolved, we can add the driver
+        if ($app->resolved('filesystem')) {
+            $this->addDriver($app->make('filesystem'), $sprout, $tracker);
+        } else {
+            // But if it hasn't, we'll add it once it is
+            $app->afterResolving('filesystem', function (FilesystemManager $manager) use ($sprout, $tracker) {
+                $this->addDriver($manager, $sprout, $tracker);
             });
+        }
+    }
+
+    protected function addDriver(FilesystemManager $manager, Sprout $sprout, Closure $tracker): void
+    {
+        $manager->extend('sprout', function (Application $app, array $config) use ($manager, $sprout, $tracker): Filesystem {
+            // If the config contains the disk name
+            if (isset($config['name'])) {
+                // Track it
+                $tracker($config['name']);
+            }
+
+            return (new SproutFilesystemDriverCreator($app, $manager, $config, $sprout))();
         });
     }
 
