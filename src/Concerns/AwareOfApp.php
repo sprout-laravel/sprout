b566bbbf1cfc2e15afb092dd27875f9da323f3ed
Ollie Read <code@ollieread.com>
2025-01-27 00:28:28 +0000

    chore: Add AwareOfApp and AwareOfSprout traits for use in overrides, identity resolvers, etc

---
+++ src/Concerns/AwareOfApp.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Sprout\Concerns;
+
+use Illuminate\Contracts\Foundation\Application;
+
+trait AwareOfApp
+{
+    /**
+     * @var \Illuminate\Contracts\Foundation\Application
+     */
+    private Application $app;
+
+    public function setApp(Application $app): static
+    {
+        $this->app = $app;
+        
+        return $this;
+    }
+
+    protected function getApp(): Application
+    {
+        return $this->app;
+    }
+}
---
+++ src/Concerns/AwareOfSprout.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace Sprout\Concerns;
+
+use Sprout\Sprout;
+
+trait AwareOfSprout
+{
+    /**
+     * @var \Sprout\Sprout
+     */
+    private Sprout $sprout;
+
+    public function setSprout(Sprout $sprout): static
+    {
+        $this->sprout = $sprout;
+        
+        return $this;
+    }
+
+    protected function getSprout(): Sprout
+    {
+        return $this->sprout;
+    }
+}
