7de0ccc5c58d7c0aaa73069d6760b85a8699df28
Ollie Read <code@ollieread.com>
2025-01-27 18:14:39 +0000

    refactor(overrides): Use the stored app and sprout instances in the cookie override

--- src/Overrides/CookieOverride.php
+++ src/Overrides/CookieOverride.php
@@ -4,8 +4,6 @@ declare(strict_types=1);
 namespace Sprout\Overrides;
 
 use Illuminate\Cookie\CookieJar;
-use Sprout\Contracts\DeferrableServiceOverride;
-use Sprout\Contracts\ServiceOverride;
 use Sprout\Contracts\Tenancy;
 use Sprout\Contracts\Tenant;
 use function Sprout\settings;
@@ -35,10 +33,10 @@ final class CookieOverride extends BaseOverride
     public function setup(Tenancy $tenancy, Tenant $tenant): void
     {
         // Collect the values
-        $path     = settings()->getUrlPath(config('session.path') ?? '/');             // @phpstan-ignore-line
-        $domain   = settings()->getUrlDomain(config('session.domain'));                // @phpstan-ignore-line
-        $secure   = settings()->shouldCookieBeSecure(config('session.secure', false)); // @phpstan-ignore-line
-        $sameSite = settings()->getCookieSameSite(config('session.same_site'));        // @phpstan-ignore-line
+        $path     = $this->getSprout()->settings()->getUrlPath(config('session.path') ?? '/');             // @phpstan-ignore-line
+        $domain   = $this->getSprout()->settings()->getUrlDomain(config('session.domain'));                // @phpstan-ignore-line
+        $secure   = $this->getSprout()->settings()->shouldCookieBeSecure(config('session.secure', false)); // @phpstan-ignore-line
+        $sameSite = $this->getSprout()->settings()->getCookieSameSite(config('session.same_site'));        // @phpstan-ignore-line
 
         /**
          * This is here to make PHPStan quiet down
@@ -50,6 +48,8 @@ final class CookieOverride extends BaseOverride
          */
 
         // Set the default values on the cookiejar
-        app(CookieJar::class)->setDefaultPathAndDomain($path, $domain, $secure, $sameSite);
+        $this->getApp()
+             ->make(CookieJar::class)
+             ->setDefaultPathAndDomain($path, $domain, $secure, $sameSite);
     }
 }
--- tests/Unit/Overrides/CookieOverrideTest.php
+++ tests/Unit/Overrides/CookieOverrideTest.php
@@ -4,25 +4,21 @@ declare(strict_types=1);
 namespace Sprout\Tests\Unit\Overrides;
 
 use GuzzleHttp\Cookie\CookieJar;
-use Illuminate\Auth\AuthManager;
 use Illuminate\Config\Repository;
-use Illuminate\Queue\Events\JobProcessing;
+use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Support\Facades\Event;
+use Mockery;
 use Mockery\MockInterface;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use Sprout\Contracts\BootableServiceOverride;
-use Sprout\Contracts\DeferrableServiceOverride;
 use Sprout\Events\ServiceOverrideBooted;
-use Sprout\Events\ServiceOverrideProcessed;
-use Sprout\Events\ServiceOverrideProcessing;
 use Sprout\Events\ServiceOverrideRegistered;
-use Sprout\Listeners\SetCurrentTenantForJob;
-use Sprout\Overrides\Auth\SproutAuthPasswordBrokerManager;
 use Sprout\Overrides\AuthOverride;
 use Sprout\Overrides\CookieOverride;
-use Sprout\Overrides\JobOverride;
-use Sprout\Overrides\StorageOverride;
+use Sprout\Sprout;
 use Sprout\Support\Services;
+use Sprout\Support\SettingsRepository;
 use Sprout\Tests\Unit\UnitTestCase;
 use Workbench\App\Models\TenantModel;
 use function Sprout\sprout;
@@ -62,82 +58,12 @@ class CookieOverrideTest extends UnitTestCase
     }
 
     #[Test]
-    public function isDeferredCorrectly(): void
-    {
-        $this->markTestSkipped('This test needs to be updated');
-
-        $sprout = sprout();
-
-        Event::fake();
-
-        $sprout->registerOverride(Services::COOKIE, CookieOverride::class);
-
-        Event::assertDispatched(ServiceOverrideRegistered::class);
-        Event::assertNotDispatched(ServiceOverrideProcessing::class);
-        Event::assertNotDispatched(ServiceOverrideProcessed::class);
-        Event::assertNotDispatched(ServiceOverrideBooted::class);
-
-        $this->assertTrue($sprout->hasRegisteredOverride(CookieOverride::class));
-        $this->assertFalse($sprout->hasOverride(CookieOverride::class));
-        $this->assertTrue($sprout->isServiceBeingOverridden(Services::COOKIE));
-        $this->assertFalse($sprout->isBootableOverride(CookieOverride::class));
-        $this->assertTrue($sprout->isDeferrableOverride(CookieOverride::class));
-        $this->assertFalse($sprout->hasBootedOverride(CookieOverride::class));
-        $this->assertFalse($sprout->hasOverrideBeenSetup(CookieOverride::class));
-
-        $overrides = $sprout->getOverrides();
-
-        $this->assertEmpty($overrides);
-
-        $overrides = $sprout->getRegisteredOverrides();
-
-        $this->assertCount(1, $overrides);
-        $this->assertContains(CookieOverride::class, $overrides);
-
-        app()->make('cookie');
-
-        $overrides = $sprout->getOverrides();
-
-        $this->assertCount(1, $overrides);
-        $this->assertInstanceOf(CookieOverride::class, $overrides[CookieOverride::class]);
-
-        $this->assertFalse($sprout->isBootableOverride(CookieOverride::class));
-        $this->assertFalse($sprout->hasBootedOverride(CookieOverride::class));
-
-        Event::assertDispatched(ServiceOverrideProcessing::class);
-        Event::assertDispatched(ServiceOverrideProcessed::class);
-        Event::assertNotDispatched(ServiceOverrideBooted::class);
-    }
-
-    #[Test]
     public function performsSetup(): void
     {
-        $this->markTestSkipped('This test needs to be updated');
+        $override = new CookieOverride('cookie', []);
 
-        $sprout = sprout();
+        $app = Mockery::mock(Application::class);
 
-        $sprout->registerOverride(Services::COOKIE, CookieOverride::class);
-
-        app()->make('cookie');
-
-        $override = $sprout->getOverrides()[CookieOverride::class];
-
-        $this->assertInstanceOf(CookieOverride::class, $override);
-
-        $this->assertNull(sprout()->settings()->getUrlPath());
-        $this->assertNull(sprout()->settings()->getUrlDomain());
-        $this->assertNull(sprout()->settings()->shouldCookieBeSecure());
-        $this->assertNull(sprout()->settings()->getCookieSameSite());
-
-        $tenant  = TenantModel::factory()->createOne();
-        $tenancy = $sprout->tenancies()->get();
-
-        $tenancy->setTenant($tenant);
-
-        $this->instance('cookie', $this->spy(CookieJar::class, function (MockInterface $mock) {
-            $mock->shouldReceive('setDefaultPathAndDomain')->once();
-        }));
-
-        $override->setup($tenancy, $tenant);
+        $sprout = new Sprout($app, new SettingsRepository());
     }
 }
