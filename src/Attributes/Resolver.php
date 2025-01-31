b1d6c9cd97147816537ac0147061d3ca06151bfb
Ollie Read <code@ollieread.com>
2025-01-31 21:08:57 +0000

    feat: Add contextual attributes for Sprouts core managers (#96)
    * chore: Make the factory get method also return null
    
    * feat: Add contextual attributes for service overrides, tenant providers, identity resolvers and tenancies
    
    * revert: "chore: Make the factory get method also return null"
    
    This reverts commit 5556c5695dc3b0c5dd44da5b83a07913ae70f6c4.

---
+++ src/Attributes/Resolver.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace Sprout\Attributes;
+
+use Attribute;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Container\ContextualAttribute;
+use Sprout\Contracts\IdentityResolver;
+use Sprout\Managers\IdentityResolverManager;
+
+/**
+ * Resolver Attribute
+ *
+ * This is a contextual attribute that allows for the auto-injection of an
+ * identity resolver using its registered name, or the default.
+ *
+ * @see     https://laravel.com/docs/11.x/container#contextual-attributes
+ *
+ * @package Core
+ */
+#[Attribute(Attribute::TARGET_PARAMETER)]
+final readonly class Resolver implements ContextualAttribute
+{
+    /**
+     * @var string|null
+     */
+    public ?string $name;
+
+    /**
+     * Create a new instance
+     *
+     * @param string|null $name
+     */
+    public function __construct(?string $name = null)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Resolve the tenancy using this attribute
+     *
+     * @param \Sprout\Attributes\Resolver     $attribute
+     * @param \Illuminate\Container\Container $container
+     *
+     * @return \Sprout\Contracts\IdentityResolver|null
+     *
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     * @throws \Sprout\Exceptions\MisconfigurationException
+     */
+    public function resolve(Resolver $attribute, Container $container): ?IdentityResolver
+    {
+        /**
+         * It's not nullable, it'll be an exception
+         *
+         * @noinspection NullPointerExceptionInspection
+         */
+        return $container->make(IdentityResolverManager::class)->get($this->name);
+    }
+}
---
+++ src/Attributes/Tenancy.php
@@ -0,0 +1,60 @@
+<?php
+declare(strict_types=1);
+
+namespace Sprout\Attributes;
+
+use Attribute;
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Container\ContextualAttribute;
+use Sprout\Contracts\Tenancy as TenancyContract;
+use Sprout\Managers\TenancyManager;
+
+/**
+ * Tenancy Attribute
+ *
+ * This is a contextual attribute that allows for the auto-injection of the
+ * tenancy, either using its registered name or the default.
+ *
+ * @see     https://laravel.com/docs/11.x/container#contextual-attributes
+ *
+ * @package Core
+ */
+#[Attribute(Attribute::TARGET_PARAMETER)]
+final readonly class Tenancy implements ContextualAttribute
+{
+    /**
+     * @var string|null
+     */
+    public ?string $name;
+
+    /**
+     * Create a new instance
+     *
+     * @param string|null $name
+     */
+    public function __construct(?string $name = null)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Resolve the tenancy using this attribute
+     *
+     * @param \Sprout\Attributes\Tenancy      $attribute
+     * @param \Illuminate\Container\Container $container
+     *
+     * @return \Sprout\Contracts\Tenancy<*>|null
+     *
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     * @throws \Sprout\Exceptions\MisconfigurationException
+     */
+    public function resolve(Tenancy $attribute, Container $container): ?TenancyContract
+    {
+        /**
+         * It's not nullable, it'll be an exception
+         *
+         * @noinspection NullPointerExceptionInspection
+         */
+        return $container->make(TenancyManager::class)->get($this->name);
+    }
+}
