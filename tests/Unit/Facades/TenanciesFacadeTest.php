6895ffbd0a05cbc71156f3191ccec2be1098dbc4
Ollie Read <code@ollieread.com>
2025-01-31 20:26:06 +0000

    feat(facades): Add a Tenancies facade for the TenancyManager

---
+++ tests/Unit/Facades/TenanciesFacadeTest.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Sprout\Tests\Unit\Facades;
+
+use PHPUnit\Framework\Attributes\Test;
+use Sprout\Facades\Sprout;
+use Sprout\Tests\Unit\UnitTestCase;
+
+class TenanciesFacadeTest extends UnitTestCase
+{
+    #[Test]
+    public function usesCorrectInstance(): void
+    {
+        $this->assertEquals(\Sprout\sprout()->tenancies(), Sprout::getFacadeRoot());
+    }
+}
